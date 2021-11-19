cc78aa211ecb8e70b46402fe329fce9ed5bac446
bg-dev <nik.azizoff2016@gmail.com>
2021-11-19 10:21:51 +0300

    add youtube logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 import '../scss/components/header.scss'
+import logo from '../assets/logo.svg'
 
 function Header() {
     return(
         <div className="header">
+            <div className="header__logo">
+                <img src={logo} width={'40px'} alt="logo" className="header__logo-pic" />
+            </div>
             <div className="header__title">
                 <h1 className="header__title-text">YouTube Video Search</h1>
             </div>
